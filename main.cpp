3570c6047f0b56e531bd3d708b51ae3aec46bfed
HowToSurvive <161419667+HowToSurvive@users.noreply.github.com>
2024-03-15 11:19:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <cstdlib>
-//ccccc
+//cccccc
 int main(int argc, char* argv[]) {
     if (argc > 1) {
         int build_number = std::atoi(argv[1]);
         std::cout << "build " << build_number << std::endl;
     }
     std::cout << "Hello, World!" << std::endl;
-    return 0;ddddd
+    return 0;
 }
